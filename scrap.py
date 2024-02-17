39be1fdd691710c95fe22d8543a973d180c5d211
Guillermo Arguello <elmemozac6@gmail.com>
2024-02-16 22:57:51 -0300

    Signed-off-by: Guillermo Arguello <elmemozac6@gmail.com>

--- scrap.py
+++
@@ -1,9 +0,0 @@
-import re 
-from colorama import Fore
-import requests
-
-website = 'https://hidrografia2.agpse.gob.ar/Ituzaingo/index.html'
-
-resultado = requests.get(website)
-content = resultado.text
-print(content)
